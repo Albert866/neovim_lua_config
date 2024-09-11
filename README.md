1f600819e9132f84f1141db35aa38b19c98ffec9
Albert <acksman86@gmail.com>
2024-09-11 20:06:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Lua config neovim IDE
+---
+
+## tree
+
+```
+├── init.lua
+├── lazy-lock.json
+├── lua
+│   ├── colorscheme.lua
+│   ├── keymaps.lua
+│   ├── options.lua
+│   └── plugins.lua
+└── Readme.md
+```
---
+++ lua/plugins.lua
@@ -0,0 +1,21 @@
+-- Install Lazy.nvim automatically if it's not installed(Bootstraping)
+-- Hint: string concatenation is done by `..`
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    -- Install vim-monokai colorscheme
+    {
+        'crusoexia/vim-monokai',
+    },
+})
