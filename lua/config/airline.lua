9a10ff2fb09ac9a331d20213c44bf6214e6c2007
Albert <acksman86@gmail.com>
2024-11-10 11:26:44 +0800

    Debug airline image display

--- lua/config/airline.lua
+++ lua/config/airline.lua
@@ -5,7 +5,7 @@ vim.g.airline_powerline_fonts = 1
 vim.g.airline_theme = "desertink"
 
 -- enable tabline extern
-vim.g['webdevicons_enable_airline_tabline'] = 0
+vim.g['webdevicons_enable_airline_tabline'] = 1
 vim.g['webdevicons_enable_airline_bufferline'] = 1
 vim.g['airline#extensions#tabline#enabled'] = 1
 vim.g['airline#extensions#tabline#left_alt_sep'] = '|'
