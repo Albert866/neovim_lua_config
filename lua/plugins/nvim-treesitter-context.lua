54f2cb2d01b78d88f8b4048f4edd4cd54dd7cf6f
Axeman <acksman86@gmail.com>
2025-04-26 09:24:41 +0800

    [update] add plugins/* plugins module

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -1,6 +1,6 @@
 return {
-	"nvim-treesitter/nvim-treesitter-context",
-	opts = {
-		max_lines = 3,
-	},
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+        max_lines = 5,
+    },
 }
