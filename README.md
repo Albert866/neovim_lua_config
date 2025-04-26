6a1d71b882225fc28d6ea409291cc65c81be8f3b
Axeman <acksman86@gmail.com>
2025-04-26 12:06:12 +0800

    [update] add readme install applicate

--- README.md
+++ README.md
@@ -47,6 +47,15 @@
 
 ![show coc lsp](./images/coc.png)
 
+## Install applicate
+
+```bash
+sudo apt install xclip bear cmake ccls clang generate-ninja ninja-build automake\
+     git gcc g++ pkg-config autoconf libssl-dev libdbus-1-dev default-jre \
+     libglib2.0-dev libavahi-client-dev ninja-build python3-venv python3-dev \
+     python3-pip unzip libgirepository1.0-dev libcairo2-dev libreadline-dev \
+```
+
 ## Install neovim
 
 - neovim stable version greater than 0.10.0
--- lua/options.lua
+++ lua/options.lua
@@ -1,15 +1,15 @@
 -- Hint: use `:h <option>` to figure out the meaning if needed
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard, need install xclip
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
-vim.opt.mouse = "" -- "a" allow the mouse to be used in Nvim, "" disable mouse
-vim.opt.scrolloff = 10 -- no less than 10 lines even if you keep scrolling down
+vim.opt.mouse = ""                -- "a" allow the mouse to be used in Nvim, "" disable mouse
+vim.opt.scrolloff = 10            -- no less than 10 lines even if you keep scrolling down
 vim.opt.hidden = true
 
 -- file type set
 -- file type check
 -- file type loading plugin
 -- auto indent
-local cmd_run = [[ 
+local cmd_run = [[
     syntax on
     filetype on
     filetype plugin on
@@ -18,28 +18,28 @@ local cmd_run = [[
 vim.cmd(cmd_run)
 
 -- Tab
-vim.opt.tabstop = 4 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4 -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
-vim.opt.expandtab = true -- tabs are spaces, mainly because of python
-vim.opt.autoindent = true  -- auto indent
+vim.opt.tabstop = 4       -- number of visual spaces per TAB
+vim.opt.softtabstop = 4   -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4    -- insert 4 spaces on a tab
+vim.opt.expandtab = true  -- tabs are spaces, mainly because of python
+vim.opt.autoindent = true -- auto indent
 
 -- UI config
-vim.opt.number = true -- show absolute number
+vim.opt.number = true         -- show absolute number
 vim.opt.relativenumber = true -- add numbers to each line on the left side
-vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true -- open new vertical split bottom
-vim.opt.splitright = true -- open new horizontal splits right
-vim.opt.termguicolors = true -- enabl 24-bit RGB color in the TUI
-vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
-vim.opt.compatible = false -- disable vi compatibility mode
+vim.opt.cursorline = true     -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true     -- open new vertical split bottom
+vim.opt.splitright = true     -- open new horizontal splits right
+vim.opt.termguicolors = true  -- enabl 24-bit RGB color in the TUI
+vim.opt.showmode = false      -- we are experienced, wo don't need the "-- INSERT --" mode hint
+vim.opt.compatible = false    -- disable vi compatibility mode
 vim.opt.laststatus = 2
 
 -- Searching
-vim.opt.incsearch = true -- search as characters are entered
-vim.opt.hlsearch = true -- do not highlight matches
+vim.opt.incsearch = true  -- search as characters are entered
+vim.opt.hlsearch = true   -- do not highlight matches
 vim.opt.ignorecase = true -- ignore case in searches by default
-vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+vim.opt.smartcase = true  -- but make it case sensitive if an uppercase is entered
 
 -- For nvim-tree
 -- disable netrw at the very start of your init.lua (strongly advised)
