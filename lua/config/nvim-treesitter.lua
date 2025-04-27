6953057ac8892088bc6f3835210cafb432c4d5f0
Axeman <acksman86@gmail.com>
2025-04-27 11:29:15 +0800

    [update] tree-sitter HighHight file size limit

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -50,7 +50,7 @@ configs.setup({
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
     -- List of parsers to ignore installing (for "all")
-    -- ignore_install = { "javascript" },
+    ignore_install = { "javascript" },
     -- If you need to change the installation directory of the parsers (see -> Advanced Setup)
     -- parser_install_dir = "/some/path/to/store/parsers",
     -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
@@ -65,13 +65,13 @@ configs.setup({
         -- if you want to disable the module for some languages you can pass a list to the `disable` option.
         -- disable = { "c", "rust" },
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        -- disable = function(lang, buf)
-        --     local max_filesize = 100 * 1024 -- 100 KB
-        --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        --     if ok and stats and stats.size > max_filesize then
-        --         return true
-        --     end
-        -- end,
+        disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
