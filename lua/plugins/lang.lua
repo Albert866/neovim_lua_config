270b97cf34fb581bbd7c63a9422ef0db4067357a
Axeman <wangzhonghua1@dreame.tech>
2025-07-10 18:33:47 +0800

    [update][plugins] add and remove plugins     * remove rust language     * add doxygen plugins

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -6,7 +6,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.python" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.git" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
   { import = "lazyvim.plugins.extras.lang.nushell" },
