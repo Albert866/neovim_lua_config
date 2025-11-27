36ff5e9589d3795e9fdb9e6128d168651aeb1969
Axeman <axeman.wang@gmail.com>
2025-11-27 19:32:23 +0800

    [Add] Added CoplilotChat plugins configure file

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,12 @@
+return {
+  "CopilotChat.nvim",
+  opts = {
+    model = "gpt-5-codex",
+    temperature = 0.1,
+    window = {
+      layout = "vertical",
+      width = 0.4,
+    },
+    auto_insert_mode = true,
+  },
+}
