aa174c66c3c7f26e8cad0cd36a12cc36b8dcb850
xlean <axeman.wang@gmail.com>
2026-09-02 14:16:06 +0800

    [markdown] 去除 markdown 列显示的警告提示 [plantuml] 去除对 plantuml 的语法支持

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -4,6 +4,20 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = {
+            "--config",
+            vim.fs.joinpath(vim.fn.stdpath("config"), ".markdownlint-cli2.yaml"),
+          },
+        },
+      },
+    },
+  },
   { import = "lazyvim.plugins.extras.lang.yaml" },
   { import = "lazyvim.plugins.extras.lang.vue" },
   { import = "lazyvim.plugins.extras.lang.python" },
