b27ad1c929772f15e1d9d068d34ab39ac733c7f5
xlean <axeman.wang@gmail.com>
2026-09-09 20:38:38 +0800

    feat(plugin):添加对 gn 类型文件的自动格式化对齐功能

--- lua/plugins/gn.lua
+++ lua/plugins/gn.lua
@@ -2,4 +2,12 @@ return {
   {
     "kalcutter/vim-gn",
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.gn = { "gn" }
+    end,
+  },
 }
